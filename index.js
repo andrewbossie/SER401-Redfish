19cedfa12d00fd31e0e619ef00c8a1e96f39c406
Andrew Antes <antesaj@gmail.com>
2018-10-10 15:54:09 +0000

    Added index.html and configured index.js to properly connect to it from a root GET request.

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => {
-    res.sendFile("index.html");
+    res.sendFile("/index.html", {
+        root: __dirname
+    });
 });
 
-app.listen(3000);
+app.listen(8080);
