63f9f9393ed37b6bffce2613d6729ea4a532ba34
Wyatt Draggoo <wdraggoo@asu.edu>
2018-11-08 22:29:17 -0800

    Initial RedFish Mockup Data creater complete. Functionality is documented in the config.js file

--- jsMockRedfishServer/rfmockdata.js
+++ jsMockRedfishServer/rfmockdata.js
@@ -8,24 +8,52 @@ var PFuncs = require('./PatternFuncs');
 
 var patternTimers = [];
 
-config.MockupData.MockupPatterns.forEach(function(value, index) {
+config.MockupData.MockupPatterns.forEach(function(mockup, index) {
+   var p = new PFuncs();
+
+   if (!typeof p[mockup.pattern]) {
+      console.log("Pattern '" + mockup.pattern + "' not implemented! Skipping " + mockup.name + ".");
+      return;
+   }
+
    if (!patternTimers[index])
       patternTimers[index] = {};
 
-   patternTimers[index].timer = setInterval(function() {
-      var jsonData = JSON.parse(fs.readFileSync(config.RedFishData.path + value.path + "index.json", 'utf-8'));
-      console.log(value.name);
+   if (mockup.min) { p.min = mockup.min; }
+   if (mockup.max) { p.max = mockup.max; }
+   if (mockup.step) { p.step = mockup.step; }
 
-      fs.writeFileSync(config.RedFishData.path + value.path + "index.json", JSON.stringify(jsonData, null, "\t"), 'utf-8', function(err) {
+   patternTimers[index].pfuncs = p;
+
+   patternTimers[index].timer = setInterval(function() {
+      var isoDTG = new Date().toISOString();
+
+      var currJSON = JSON.parse(fs.readFileSync(config.RedFishData.path + mockup.path + "index.json", 'utf-8'));
+
+      var templateJSON = JSON.stringify(mockup.MetricValueTemplate);
+
+      templateJSON = templateJSON.replace(/#value/g,
+         patternTimers[index].pfuncs[mockup.pattern]()
+      );
+      templateJSON = templateJSON.replace(/#timestamp/g, isoDTG);
+
+      var template = JSON.parse(templateJSON);
+
+      var tmpMVs = currJSON.MetricValues.filter(function(mval) {
+         return (mval.MemberID !== template.MemberID) ||
+            (mval.MetricProperty !== template.MetricProperty);
+      });
+
+      currJSON.MetricValues = tmpMVs;
+
+      currJSON.MetricValues.push(template);
+
+      console.log(isoDTG + ": " + mockup.name + "(" + patternTimers[index].pfuncs.value + ")");
+
+      fs.writeFileSync(config.RedFishData.path + mockup.path + "index.json", JSON.stringify(currJSON, null, "\t"), 'utf-8', function(err) {
          if (err) {
             return console.log(err);
          }
       });
-   }, value.timedelay * 1000);
+   }, mockup.timedelay * 1000);
 });
-
-/*
-for (var pattern in config.MockupData.MockupPatterns) {
-   console.log(util.inspect(pattern));
-}
-*/
