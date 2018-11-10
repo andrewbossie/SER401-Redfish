77e6ac319eca1f647df6c669c07b05309be8abbf
Andrew Bossie <andrewbossie@Andrews-MacBook-Pro.local>
2018-11-10 16:42:13 -0700

    remove uncommented

--- routes/baseRoutes.js
+++ routes/baseRoutes.js
@@ -6,33 +6,5 @@ module.exports = app => {
   app.use(express.static("./Resources"));
 
   app.get("/", app_controller.getPanels);
-// => {
-    // res.render("index.hbs", {
-    //   pageTitle: "Redfish Telemetry Client (Grafana)",
-    //   currentYear: new Date().getFullYear(),
-    //   panels: [
-    //     {
-    //       src:
-    //         "http://52.37.217.87:3000/d-solo/uiNmWixmz/randomdata?refresh=5s&orgId=1&panelId=2&var-Host=serverB",
-    //       label: "Static Grafana Panel 1"
-    //     },
-    //     {
-    //       src:
-    //         "http://52.37.217.87:3000/d-solo/uiNmWixmz/randomdata?refresh=5s&orgId=1&panelId=2&var-Host=serverA",
-    //       label: "Static Grafana Panel 2"
-    //     },
-    //     {
-    //       src:
-    //         "http://52.37.217.87:3000/d-solo/uiNmWixmz/randomdata?refresh=5s&orgId=1&var-Host=serverA&panelId=6",
-    //       label: "Static Grafana Panel 3"
-    //     },
-    //     {
-    //       src:
-    //         "http://52.37.217.87:3000/d-solo/uiNmWixmz/randomdata?refresh=5s&orgId=1&panelId=4&var-Host=serverB",
-    //       label: "Static Grafana Panel 4"
-    //     }
-    //   ]
-    // });
-  // });
   app.get('/charts', app_controller.getInfluxData);
 };
