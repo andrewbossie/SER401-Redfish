4c04bddda36ff7d353e14eaf60770efeda8bae68
Maigan <maigansedate@gmail.com>
2018-11-29 20:06:38 -0800

    Unifying the style in the repo to keep things consistent.

--- Controllers/AppController.js
+++ Controllers/AppController.js
@@ -1,195 +1,193 @@
 const keys = require("../config/keys");
 const request = require("request");
 const Influx = require("influx");
-
 const util = require("../Resources/js/util");
 const rTools = require("../Resources/js/redfishTools");
 
 // Get CPU data
 let metrics = {
-  cpuUtil_1: {
-    date: null,
-    timestamp: null,
-    metric: null
-  },
-  cpuUtil_2: {
-    date: null,
-    timestamp: null,
-    metric: null
-  },
-  cpuUtil_3: {
-    date: null,
-    timestamp: null,
-    metric: null
-  }
+   cpuUtil_1: {
+      date: null,
+      timestamp: null,
+      metric: null
+   },
+   cpuUtil_2: {
+      date: null,
+      timestamp: null,
+      metric: null
+   },
+   cpuUtil_3: {
+      date: null,
+      timestamp: null,
+      metric: null
+   }
 };
 
 let updateMetric = (target, newMetric) => {
-  let date = new Date(util.convertToIsoDate(newMetric.TimeStamp));
-  d2 = new Date();
-  now = d2.getSeconds();
-  date.setMinutes(date.getMinutes() + now);
+   let date = new Date(util.convertToIsoDate(newMetric.TimeStamp));
+   d2 = new Date();
+   now = d2.getSeconds();
+   date.setMinutes(date.getMinutes() + now);
 
-  target.timestamp = Influx.toNanoDate(date);
-  target.metric = newMetric.MetricValue;
+   target.timestamp = Influx.toNanoDate(date);
+   target.metric = newMetric.MetricValue;
 };
 
 // Update influx from API
 exports.updateCPUUtil = () => {
-  request(
-    {
-      url:
-        "http://localhost:8000/redfish/v1/TelemetryService/MetricReports/CPUMetrics",
-      json: true
-    },
-    (error, response, body) => {
-      if (error) {
-        console.log("Unable to connect to server.");
-      } else {
-        if (body.MetricValues) {
-          let cpuUtil_1 = rTools.getMetric(
-            body.MetricValues,
-            "CPUPercentUtil",
-            1,
-            1
-          );
-          let cpuUtil_2 = rTools.getMetric(
-            body.MetricValues,
-            "CPUPercentUtil",
-            1,
-            2
-          );
-          let cpuUtil_3 = rTools.getMetric(
-            body.MetricValues,
-            "CPUPercentUtil",
-            1,
-            3
-          );
+   request(
+      {
+         url:
+            "http://localhost:800/redfish/v1/TelemetryService/MetricReports/CPUMetrics",
+         json: true
+      },
+      (error, response, body) => {
+         if (error) {
+            console.log("Unable to connect to server.");
+         } else {
+            if (body.MetricValues) {
+               let cpuUtil_1 = rTools.getMetric(
+                  body.MetricValues,
+                  "CPUPercentUtil",
+                  1,
+                  1
+               );
+               let cpuUtil_2 = rTools.getMetric(
+                  body.MetricValues,
+                  "CPUPercentUtil",
+                  1,
+                  2
+               );
+               let cpuUtil_3 = rTools.getMetric(
+                  body.MetricValues,
+                  "CPUPercentUtil",
+                  1,
+                  3
+               );
 
-          updateMetric(metrics.cpuUtil_1, cpuUtil_1);
-          updateMetric(metrics.cpuUtil_2, cpuUtil_2);
-          updateMetric(metrics.cpuUtil_3, cpuUtil_3);
-        }
+               updateMetric(metrics.cpuUtil_1, cpuUtil_1);
+               updateMetric(metrics.cpuUtil_2, cpuUtil_2);
+               updateMetric(metrics.cpuUtil_3, cpuUtil_3);
+            }
+         }
       }
-    }
-  );
+   );
 };
 
 // InfluxDB Connection
 const influx = new Influx.InfluxDB({
-  host: keys.influxHost,
-  database: "test",
-  username: keys.influxUserName,
-  password: keys.influxPassword,
+   host: keys.influxHost,
+   database: "test",
+   username: keys.influxUserName,
+   password: keys.influxPassword,
 
-  schema: [
-    {
-      measurement: "CPUPercentUtil",
-      fields: { value: Influx.FieldType.FLOAT },
-      tags: ["host", "tray", "id"]
-    },
-    {
-      measurement: "temp",
-      fields: { value: Influx.FieldType.FLOAT },
-      tags: ["host"]
-    }
-  ]
+   schema: [
+      {
+         measurement: "CPUPercentUtil",
+         fields: { value: Influx.FieldType.FLOAT },
+         tags: ["host", "tray", "id"]
+      },
+      {
+         measurement: "temp",
+         fields: { value: Influx.FieldType.FLOAT },
+         tags: ["host"]
+      }
+   ]
 });
 
 // Random test data (DEPRECATED)
 exports.writeDataTest = function() {
-  // console.log(metrics.cpuUtil.timestamp.getNanoTime());
-  influx
-    .writePoints(
-      [
-        {
-          measurement: "CPUPercentUtil",
-          tags: { host: "serverA", tray: 1, id: 1 },
-          fields: { value: metrics.cpuUtil_1.metric },
-          timestamp: metrics.cpuUtil_1.timestamp.getNanoTime()
-        },
-        {
-          measurement: "CPUPercentUtil",
-          tags: { host: "serverA", tray: 1, id: 2 },
-          fields: { value: metrics.cpuUtil_2.metric },
-          timestamp: metrics.cpuUtil_2.timestamp.getNanoTime()
-        },
-        {
-          measurement: "CPUPercentUtil",
-          tags: { host: "serverA", tray: 1, id: 3 },
-          fields: { value: metrics.cpuUtil_3.metric },
-          timestamp: metrics.cpuUtil_3.timestamp.getNanoTime()
-        },
-        {
-          measurement: "cpu",
-          tags: { host: "serverB" },
-          fields: { value: Math.random() * 75 }
-        },
-        {
-          measurement: "temp",
-          tags: { host: "serverA" },
-          fields: { value: Math.random() * 200 }
-        }
-      ],
-      {
-        database: "test",
-        precision: "s"
-      }
-    )
-    .catch(err => {
-      console.error(`Error writing data to Influx. ${err.stack}`);
-    });
+   influx
+      .writePoints(
+         [
+            {
+               measurement: "CPUPercentUtil",
+               tags: { host: "serverA", tray: 1, id: 1 },
+               fields: { value: metrics.cpuUtil_1.metric },
+               timestamp: metrics.cpuUtil_1.timestamp.getNanoTime()
+            },
+            {
+               measurement: "CPUPercentUtil",
+               tags: { host: "serverA", tray: 1, id: 2 },
+               fields: { value: metrics.cpuUtil_2.metric },
+               timestamp: metrics.cpuUtil_2.timestamp.getNanoTime()
+            },
+            {
+               measurement: "CPUPercentUtil",
+               tags: { host: "serverA", tray: 1, id: 3 },
+               fields: { value: metrics.cpuUtil_3.metric },
+               timestamp: metrics.cpuUtil_3.timestamp.getNanoTime()
+            },
+            {
+               measurement: "cpu",
+               tags: { host: "serverB" },
+               fields: { value: Math.random() * 75 }
+            },
+            {
+               measurement: "temp",
+               tags: { host: "serverA" },
+               fields: { value: Math.random() * 200 }
+            }
+         ],
+         {
+            database: "test",
+            precision: "s"
+         }
+      )
+      .catch(err => {
+         console.error(`Error writing data to Influx. ${err.stack}`);
+      });
 };
 
 // Render Static Panels in Grafana
 exports.getPanels = function(req, res) {
-  res.render("index.hbs", {
-    pageTitle: "Redfish Telemetry Client",
-    currentYear: new Date().getFullYear()
-  });
+   res.render("index.hbs", {
+      pageTitle: "Redfish Telemetry Client",
+      currentYear: new Date().getFullYear()
+   });
 };
 
 // Grab Influx Data. Can we do this without nesting?
 exports.getInfluxData = function(req, res) {
-  var cpu = [];
-  var temp = [];
-  var cpu_time = [];
-  var temp_time = [];
+   var cpu = [];
+   var temp = [];
+   var cpu_time = [];
+   var temp_time = [];
 
-  // Get CPU Metrics
-  let getCPUData = influx.query("SELECT * FROM cpu").catch(err => {
-    console.error(`Error retrieving data from Influx. ${err.stack}`);
-  });
+   // Get CPU Metrics
+   let getCPUData = influx.query("SELECT * FROM cpu").catch(err => {
+      console.error(`Error retrieving data from Influx. ${err.stack}`);
+   });
 
-  let getTempData = influx.query("SELECT * FROM temp").catch(err => {
-    console.error(`Error retrieving data from Influx. ${err.stack}`);
-  });
+   let getTempData = influx.query("SELECT * FROM temp").catch(err => {
+      console.error(`Error retrieving data from Influx. ${err.stack}`);
+   });
 
-  // Promise.all allows us to wait for all calls to resolve. This way, we don't need to nest callbacks
-  Promise.all([getCPUData, getTempData]).then(results => {
-    for (var i = 0; i < results[0].length; i++) {
-      cpu_time[i] = results[0][i]["time"];
-      cpu[i] = results[0][i]["value"];
-    }
+   // Promise.all allows us to wait for all calls to resolve. This way, we don't need to nest callbacks
+   Promise.all([getCPUData, getTempData]).then(results => {
+      for (var i = 0; i < results[0].length; i++) {
+         cpu_time[i] = results[0][i]["time"];
+         cpu[i] = results[0][i]["value"];
+      }
 
-    for (var i = 0; i < results[1].length; i++) {
-      temp_time[i] = results[1][i]["time"];
-      temp[i] = results[1][i]["value"];
-    }
+      for (var i = 0; i < results[1].length; i++) {
+         temp_time[i] = results[1][i]["time"];
+         temp[i] = results[1][i]["value"];
+      }
 
-    res.render("chart.hbs", {
-      pageTitle: "Redfish Telemetry Client (Js)",
-      cpu: cpu,
-      temp: temp,
-      cpu_time: cpu_time,
-      temp_time: temp_time
-    });
-  });
+      res.render("chart.hbs", {
+         pageTitle: "Redfish Telemetry Client (Js)",
+         cpu: cpu,
+         temp: temp,
+         cpu_time: cpu_time,
+         temp_time: temp_time
+      });
+   });
 };
 /*
-* Note: Redfish API Specification DateTime values are in ISO 8601 "extended"
-*  format: ex. "2017-11-23T17:17:42-0600"
-*
-*   Current RedishMockupServer metric reports contain format
-*     "20161108T142504-0500"
-*/
+ * Note: Redfish API Specification DateTime values are in ISO 8601 "extended"
+ *  format: ex. "2017-11-23T17:17:42-0600"
+ *
+ *   Current RedishMockupServer metric reports contain format
+ *     "20161108T142504-0500"
+ */
--- jsMockRedfishServer/rfmockdata.js
+++ jsMockRedfishServer/rfmockdata.js
@@ -1,10 +1,10 @@
 "use strict";
 
-var util = require('util');
-var fs = require('fs');
+var util = require("util");
+var fs = require("fs");
 
 var config;
-var PFuncs = require('./PatternFuncs');
+var PFuncs = require("./PatternFuncs");
 var mode = "";
 
 if (process.argv.indexOf("-c") != -1) {
@@ -23,12 +23,12 @@ if (process.argv.indexOf("-c") != -1) {
 if (process.argv.indexOf("-f") != -1) {
    if (process.argv[process.argv.indexOf("-f") + 1] != -1) {
       //write data to file
-	  mode = "file";
+      mode = "file";
    }
 }
 
 if (!config) {
-   config = require('./config');
+   config = require("./config");
 }
 
 if (process.argv.indexOf("-s") != 1 && process.argv.indexOf("-f") == -1) {
@@ -36,10 +36,10 @@ if (process.argv.indexOf("-s") != 1 && process.argv.indexOf("-f") == -1) {
 }
 
 function startServer(redfishPath) {
-   var express = require('express');
+   var express = require("express");
    var app = express();
 
-   app.use('/redfish', express.static(redfishPath, {index: "index.json"}));
+   app.use("/redfish", express.static(redfishPath, { index: "index.json" }));
 
    app.listen(8001);
 
@@ -49,142 +49,173 @@ function startServer(redfishPath) {
 var patternTimers = [];
 
 //for live data
-if (mode != "file"){
-	config.MockupData.MockupPatterns.forEach(function(mockup, index) {
-	   var p = new PFuncs();
+if (mode != "file") {
+   config.MockupData.MockupPatterns.forEach(function(mockup, index) {
+      var p = new PFuncs();
 
-	   if (!typeof p[mockup.pattern]) {
-		  console.log("Pattern '" + mockup.pattern + "' not implemented! Skipping " + mockup.name + ".");
-		  return;
-	   }
+      if (!typeof p[mockup.pattern]) {
+         console.log(
+            "Pattern '" +
+               mockup.pattern +
+               "' not implemented! Skipping " +
+               mockup.name +
+               "."
+         );
+         return;
+      }
 
-	   if (!patternTimers[index])
-		  patternTimers[index] = {};
+      if (!patternTimers[index]) patternTimers[index] = {};
 
-	   if (mockup.min) { p.min = mockup.min; }
-	   if (mockup.max) { p.max = mockup.max; }
-	   if (mockup.step) { p.step = mockup.step; }
+      if (mockup.min) {
+         p.min = mockup.min;
+      }
+      if (mockup.max) {
+         p.max = mockup.max;
+      }
+      if (mockup.step) {
+         p.step = mockup.step;
+      }
 
-	   patternTimers[index].pfuncs = p;
+      patternTimers[index].pfuncs = p;
 
-	   patternTimers[index].timer = setInterval(function() {
-		  var isoDTG = new Date().toISOString();
-		  isoDTG = isoDTG.replace(/\..*$/, '-0500');
-		  
-		  var currJSON = JSON.parse(fs.readFileSync(config.RedFishData.path + mockup.path + "index.json", 'utf-8'));
+      patternTimers[index].timer = setInterval(function() {
+         var isoDTG = new Date().toISOString();
+         isoDTG = isoDTG.replace(/\..*$/, "-0500");
 
-		  var templateJSON = JSON.stringify(mockup.MetricValueTemplate);
+         var currJSON = JSON.parse(
+            fs.readFileSync(
+               config.RedFishData.path + mockup.path + "index.json",
+               "utf-8"
+            )
+         );
 
-		  templateJSON = templateJSON.replace(/#value/g,
-			 patternTimers[index].pfuncs[mockup.pattern]()
-		  );
-		  templateJSON = templateJSON.replace(/#timestamp/g, isoDTG);
+         var templateJSON = JSON.stringify(mockup.MetricValueTemplate);
 
-		  var template = JSON.parse(templateJSON);
+         templateJSON = templateJSON.replace(
+            /#value/g,
+            patternTimers[index].pfuncs[mockup.pattern]()
+         );
+         templateJSON = templateJSON.replace(/#timestamp/g, isoDTG);
 
-		  var tmpMVs = currJSON.MetricValues.filter(function(mval) {
-			 return (mval.MemberID !== template.MemberID) ||
-				(mval.MetricProperty !== template.MetricProperty);
-		  });
+         var template = JSON.parse(templateJSON);
 
-		  currJSON.MetricValues = tmpMVs;
+         var tmpMVs = currJSON.MetricValues.filter(function(mval) {
+            return (
+               mval.MemberID !== template.MemberID ||
+               mval.MetricProperty !== template.MetricProperty
+            );
+         });
 
-		  currJSON.MetricValues.push(template);
+         currJSON.MetricValues = tmpMVs;
 
-		  console.log(isoDTG + ": " + mockup.name + "(" + patternTimers[index].pfuncs.value + ")");
+         currJSON.MetricValues.push(template);
 
-		  fs.writeFileSync(config.RedFishData.path + mockup.path + "index.json", JSON.stringify(currJSON, null, "\t"), 'utf-8', function(err) {
-			 if (err) {
-				return console.log(err);
-			 }
-		  });
-	   }, mockup.timedelay * 1000);
-	});
+         console.log(
+            isoDTG +
+               ": " +
+               mockup.name +
+               "(" +
+               patternTimers[index].pfuncs.value +
+               ")"
+         );
+
+         fs.writeFileSync(
+            config.RedFishData.path + mockup.path + "index.json",
+            JSON.stringify(currJSON, null, "\t"),
+            "utf-8",
+            function(err) {
+               if (err) {
+                  return console.log(err);
+               }
+            }
+         );
+      }, mockup.timedelay * 1000);
+   });
 }
 
 //for saving data to files
 
-if (mode == "file"){
-	
-	
-	var isoDTG = Date.now()
-	
-	var iterations = 60 * 60;	//1 hour in seconds
-	var str = "#\n";
-	fs.writeFileSync("data.csv",str); //empty file if it already exists
-	console.log("Generating... ");
-	
-	//var lineNum = 1;
-	
-	for (var i = 1; i <= iterations; i++){
-		
-		var line = "";
-		config.MockupData.MockupPatterns.forEach(function(mockup, index) {
-				//console.log(index);
-				var currJSON = JSON.parse(fs.readFileSync(config.RedFishData.path + mockup.path + "index.json", 'utf-8'));
-				
-				
-				
-				if (patternTimers[index] == undefined)
-				  patternTimers[index] = {};
+if (mode == "file") {
+   var isoDTG = Date.now();
 
-				if(patternTimers[index].pfuncs == undefined){
-					var p = new PFuncs();
-					if (mockup.min) { p.min = mockup.min; }
-					if (mockup.max) { p.max = mockup.max; }
-					if (mockup.step) { p.step = mockup.step; }
-					patternTimers[index].pfuncs = p;
-				}
-			
-				//console.log(i);
-				var templateJSON = JSON.stringify(mockup.MetricValueTemplate);
-				
-				templateJSON = templateJSON.replace(/#value/g,
-					patternTimers[index].pfuncs[mockup.pattern]()
-				);
-				//console.log(index + ": " + patternTimers[index].pfuncs[mockup.pattern]());
-				
-				templateJSON = templateJSON.replace(/#timestamp/g, new Date(isoDTG + i*1000).toISOString().replace(/\..*$/, '-0500'));
+   var iterations = 60 * 60; //1 hour in seconds
+   var str = "#\n";
+   fs.writeFileSync("data.csv", str); //empty file if it already exists
+   console.log("Generating... ");
 
-				var template = JSON.parse(templateJSON);
+   //var lineNum = 1;
 
-				var tmpMVs = currJSON.MetricValues.filter(function(mval) {
-				 return (mval.MemberID !== template.MemberID) ||
-					(mval.MetricProperty !== template.MetricProperty);
-				});
-				
-				currJSON.MetricValues = tmpMVs;
-				currJSON.MetricValues.push(template);
-				
-				
-				
-			if(i % (mockup.timedelay) == 0){
-				line += (currJSON.Name + ",");
-				line += (template.MemberID + ",");
-				line += (template.MetricValue + ",");
-				line += (template.TimeStamp + ",");
-				line += (template.MetricProperty + ",");
+   for (var i = 1; i <= iterations; i++) {
+      var line = "";
+      config.MockupData.MockupPatterns.forEach(function(mockup, index) {
+         //console.log(index);
+         var currJSON = JSON.parse(
+            fs.readFileSync(
+               config.RedFishData.path + mockup.path + "index.json",
+               "utf-8"
+            )
+         );
 
+         if (patternTimers[index] == undefined) patternTimers[index] = {};
 
-			}
-			
-			
-			
-		});
-		if (line != ""){
-			str += (i + ",");
-			str += ("Metric,");
-			
-			str += line;
-			str += "\n";
-			//lineNum++;
-		}
-		
-	
-	}
-	str += "0,END";
-	fs.writeFileSync("data.csv", str);
-	console.log("Completed in " + (Date.now() - isoDTG) + "ms");
-	
+         if (patternTimers[index].pfuncs == undefined) {
+            var p = new PFuncs();
+            if (mockup.min) {
+               p.min = mockup.min;
+            }
+            if (mockup.max) {
+               p.max = mockup.max;
+            }
+            if (mockup.step) {
+               p.step = mockup.step;
+            }
+            patternTimers[index].pfuncs = p;
+         }
+
+         //console.log(i);
+         var templateJSON = JSON.stringify(mockup.MetricValueTemplate);
+
+         templateJSON = templateJSON.replace(
+            /#value/g,
+            patternTimers[index].pfuncs[mockup.pattern]()
+         );
+         //console.log(index + ": " + patternTimers[index].pfuncs[mockup.pattern]());
+
+         templateJSON = templateJSON.replace(
+            /#timestamp/g,
+            new Date(isoDTG + i * 1000).toISOString().replace(/\..*$/, "-0500")
+         );
+
+         var template = JSON.parse(templateJSON);
+
+         var tmpMVs = currJSON.MetricValues.filter(function(mval) {
+            return (
+               mval.MemberID !== template.MemberID ||
+               mval.MetricProperty !== template.MetricProperty
+            );
+         });
+
+         currJSON.MetricValues = tmpMVs;
+         currJSON.MetricValues.push(template);
+
+         if (i % mockup.timedelay == 0) {
+            line += currJSON.Name + ",";
+            line += template.MemberID + ",";
+            line += template.MetricValue + ",";
+            line += template.TimeStamp + ",";
+            line += template.MetricProperty + ",";
+         }
+      });
+      if (line != "") {
+         str += i + ",";
+         str += "Metric,";
+
+         str += line;
+         str += "\n";
+         //lineNum++;
+      }
+   }
+   str += "0,END";
+   fs.writeFileSync("data.csv", str);
+   console.log("Completed in " + (Date.now() - isoDTG) + "ms");
 }
-
