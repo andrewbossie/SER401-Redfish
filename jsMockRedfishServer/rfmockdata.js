5bd90493e9b6d16765172fc566460449289e9e51
jakistle <mychoosebooks@gmail.com>
2019-01-12 15:03:36 -0700

    Optimized Mockup Data File Producer by caching loaded templates and removing unecessary operations.
    Reduced generation time for 1 hour of data from 1275ms to 60ms (~95% reduction)

--- jsMockRedfishServer/rfmockdata.js
+++ jsMockRedfishServer/rfmockdata.js
@@ -136,86 +136,88 @@ if (mode != "file") {
 //for saving data to files
 
 if (mode == "file") {
-   var isoDTG = Date.now();
+	var parsedPaths = [];
+	var parsedTemplates = [];
+	var isoDTG = Date.now();
 
-   var iterations = 60 * 60; //1 hour in seconds
-   var str = "#\n";
-   fs.writeFileSync("data.csv", str); //empty file if it already exists
-   console.log("Generating... ");
+	var iterations = 60 * 60; //1 hour in seconds TODO: make this variable 
+	var str = "#\n";
+	fs.writeFileSync("data.csv", str); //empty file if it already exists
+	console.log("Generating... ");
 
-   //var lineNum = 1;
+	for (var i = 1; i <= iterations; i++) {
+		var line = "";
+		config.MockupData.MockupPatterns.forEach(function(mockup, index) {
+			//only do anything if the current iteration is on the approriate time
+			if (i % mockup.timedelay == 0) {
+				
+				//check if template has been parsed
+				let path = config.RedFishData.path + mockup.path + "index.json";
+				
+				//if its not loaded yet, load and parse the template
+				if(parsedPaths.indexOf(path) < 0){
+					let file = fs.readFileSync(path,"utf-8")
+					var currJSON = JSON.parse(file);
+					//push template to loaded templates
+					parsedTemplates.push(file);
+					parsedPaths.push(path);
+				}else{
+					//or use the loaded one
+					var currJSON = JSON.parse(parsedTemplates[parsedPaths.indexOf(path)]);
+				}
+				
 
-   for (var i = 1; i <= iterations; i++) {
-      var line = "";
-      config.MockupData.MockupPatterns.forEach(function(mockup, index) {
-         //console.log(index);
-         var currJSON = JSON.parse(
-            fs.readFileSync(
-               config.RedFishData.path + mockup.path + "index.json",
-               "utf-8"
-            )
-         );
+				if (patternTimers[index] == undefined) patternTimers[index] = {};
 
-         if (patternTimers[index] == undefined) patternTimers[index] = {};
+				if (patternTimers[index].pfuncs == undefined) {
+					var p = new PFuncs();
+					if (mockup.min) {
+						p.min = mockup.min;
+					}
+					if (mockup.max) {
+						p.max = mockup.max;
+					}
+					if (mockup.step) {
+						p.step = mockup.step;
+					}
+					patternTimers[index].pfuncs = p;
+				}
 
-         if (patternTimers[index].pfuncs == undefined) {
-            var p = new PFuncs();
-            if (mockup.min) {
-               p.min = mockup.min;
-            }
-            if (mockup.max) {
-               p.max = mockup.max;
-            }
-            if (mockup.step) {
-               p.step = mockup.step;
-            }
-            patternTimers[index].pfuncs = p;
-         }
 
-         //console.log(i);
-         var templateJSON = JSON.stringify(mockup.MetricValueTemplate);
+				var templateJSON = JSON.stringify(mockup.MetricValueTemplate);
 
-         templateJSON = templateJSON.replace(
-            /#value/g,
-            patternTimers[index].pfuncs[mockup.pattern]()
-         );
-         //console.log(index + ": " + patternTimers[index].pfuncs[mockup.pattern]());
+				templateJSON = templateJSON.replace(
+				/#value/g,
+				patternTimers[index].pfuncs[mockup.pattern]()
+				);
 
-         templateJSON = templateJSON.replace(
-            /#timestamp/g,
-            new Date(isoDTG + i * 1000).toISOString().replace(/\..*$/, "-0500")
-         );
+				templateJSON = templateJSON.replace(
+				/#timestamp/g,
+				new Date(isoDTG + i * 1000).toISOString().replace(/\..*$/, "-0500")
+				);
 
-         var template = JSON.parse(templateJSON);
+				var template = JSON.parse(templateJSON);
+				
+				//build csv record
+				line += currJSON.Name + ",";
+				line += template.MemberID + ",";
+				line += template.MetricValue + ",";
+				line += template.TimeStamp + ",";
+				line += template.MetricProperty + ",";
+			}
+		});
+		
+		//if the line is not empty, add some boilerplate and add to master string
+		if (line != "") {
+			str += i + ",";
+			str += "Metric,";
 
-         var tmpMVs = currJSON.MetricValues.filter(function(mval) {
-            return (
-               mval.MemberID !== template.MemberID ||
-               mval.MetricProperty !== template.MetricProperty
-            );
-         });
-
-         currJSON.MetricValues = tmpMVs;
-         currJSON.MetricValues.push(template);
-
-         if (i % mockup.timedelay == 0) {
-            line += currJSON.Name + ",";
-            line += template.MemberID + ",";
-            line += template.MetricValue + ",";
-            line += template.TimeStamp + ",";
-            line += template.MetricProperty + ",";
-         }
-      });
-      if (line != "") {
-         str += i + ",";
-         str += "Metric,";
-
-         str += line;
-         str += "\n";
-         //lineNum++;
-      }
-   }
-   str += "0,END";
-   fs.writeFileSync("data.csv", str);
-   console.log("Completed in " + (Date.now() - isoDTG) + "ms");
+			str += line;
+			str += "\n";
+			//lineNum++;
+		}
+	}
+	str += "0,END";
+	fs.writeFileSync("data.csv", str);
+	console.log("Completed in " + (Date.now() - isoDTG) + "ms");
 }
