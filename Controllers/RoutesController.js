0c4544a5bd1d77bc1e8706ccfbe6a099b3398a14
Andrew Antes <antesaj@gmail.com>
2019-01-17 19:38:21 -0800

    Added proper error handling to the HTTP routes. Added some documentation to the code as well. These routes need to be fixed to reach out to the proper endpoint and retrieve definitions, not metric reports themselves. Pending JSON fixes.

--- Controllers/RoutesController.js
+++ Controllers/RoutesController.js
@@ -10,6 +10,10 @@ exports.getPanels = function(req, res) {
 
 // Route handler for /metrics
 // This will return a JSON array of URIs to each available metric report.
+
+// TODO: Once the metric definition JSON files are fixed, change
+// what's being returned here. We want definition returns, not actual
+// metric report returns. That's saved for InfluxDB.
 exports.getAvailableMetrics = function(req, res) {
   request(
     {
@@ -17,7 +21,12 @@ exports.getAvailableMetrics = function(req, res) {
       json: true
     },
     (error, response, body) => {
-      if (error) {
+      if (!body) {
+        res.status(404);
+        res.json({
+          error: "Could not retrieve metrics"
+        });
+      } else if (error) {
         console.log(error);
       } else {
         let metrics = [];
@@ -33,16 +42,23 @@ exports.getAvailableMetrics = function(req, res) {
 };
 
 // Route handler for /metrics/:metric
+// Note: the mockup server isn't sending proper HTTP responses for
+// bad requests. This is why !body is checked after the request is made.
 exports.getMetric = function(req, res) {
   let metric = req.params.metric;
-  console.log(metric);
+  console.log(typeof metric);
   request(
     {
       url: `http://localhost:8000/redfish/v1/TelemetryService/MetricReports/${metric}`,
       json: true
     },
     (error, response, body) => {
-      if (error) {
+      if (!body) {
+        res.status(404);
+        res.json({
+          error: "Metric does not exist"
+        });
+      } else if (error) {
         console.log(error);
       } else {
         console.log(body);
