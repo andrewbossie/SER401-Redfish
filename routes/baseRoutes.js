8c23eceeaa897db87868e0aed60c4c7e97c96b31
Andrew Bossie <andrewbossie@Andrews-MacBook-Pro.local>
2019-01-19 19:53:16 -0700

    refactor routes 147

--- routes/baseRoutes.js
+++ routes/baseRoutes.js
@@ -4,7 +4,7 @@ const routes_controller = require("../Controllers/RoutesController");
 
 module.exports = app => {
   app.use(express.static("./Resources"));
-  app.get("/", routes_controller.getPanels);
-  app.get("/metrics", routes_controller.getAvailableMetrics);
-  app.get("/metrics/:metric", routes_controller.getMetric);
+  app.get("/panels", routes_controller.getPanels);
+  app.get("/", routes_controller.getAvailableMetrics);
+  app.get("/:metric", routes_controller.getMetric);
 };
