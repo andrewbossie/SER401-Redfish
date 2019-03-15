82aec6381506ca4239bf83b28c06ad21d25d512d
Wyatt Draggoo <wdraggoo@asu.edu>
2019-03-14 21:27:01 -0700

    Fixed the CSS alignment in the tables, with some initial coloring.

--- rfInsight/resources/js/rfModeller.js
+++ rfInsight/resources/js/rfModeller.js
@@ -22,21 +22,19 @@ function populatePatterns() {
   $.each(patternData["MockupData"]["MockupPatterns"], function(index, value) {
     console.log(value);
     html += `<div class="pattern">`;
-	html += `  <table>`;
-    html += `  <tr><td><div class="pattFormRow"><label>Name</label></td><td><input name="patName" value="` + value["name"] + `" type="text" required=true size=80 unique/></div></td></tr>`;
-    html += `  <tr><td><div class="pattFormRow"><label>Path</label></td><td><input name="patPath" value="` + value["path"] + `" type="text" required=true size=80 /></div></td></tr>`;
-    html += `  <tr><td><div class="pattFormRow"><label>Pattern</label></td><td><input name="patPatt" value="` + value["pattern"] + `" type="text" required=true size=80 /></div></td></tr>`;
-    html += `  <tr><td><div class="pattFormRow"><label>Delay</label></td><td><input name="patDelay" value="` + (typeof value["timedelay"] === "undefined" ? "15" : value["timedelay"]) + `" type="number" required=true size=20 /></div></td></tr>`;
-    html += `  <tr><td><div class="pattFormRow"><label>Minimum</label></td><td><input name="patMin" value="` + (typeof value["min"] === "undefined" ? "0" : value["min"]) + `" type="number" required=true size=20 /></div></td></tr>`;
-    html += `  <tr><td><div class="pattFormRow"><label>Step</label></td><td><input name="patStep" value="` + (typeof value["step"] === "undefined" ? "1" : value["step"]) + `" type="number" required=true size=20 /></div></td></tr>`;
+    html += `  <div class="pattFormRow"><label>Name</label><input name="patName" value="` + value["name"] + `" type="text" required=true size=80 unique/></div>`;
+    html += `  <div class="pattFormRow"><label>Path</label><input name="patPath" value="` + value["path"] + `" type="text" required=true size=80 /></div>`;
+    html += `  <div class="pattFormRow"><label>Pattern</label><input name="patPatt" value="` + value["pattern"] + `" type="text" required=true size=80 /></div>`;
+    html += `  <div class="pattFormRow"><label>Delay</label><input name="patDelay" value="` + (typeof value["timedelay"] === "undefined" ? "15" : value["timedelay"]) + `" type="number" required=true size=20 /></div>`;
+    html += `  <div class="pattFormRow"><label>Minimum</label><input name="patMin" value="` + (typeof value["min"] === "undefined" ? "0" : value["min"]) + `" type="number" required=true size=20 /></div>`;
+    html += `  <div class="pattFormRow"><label>Step</label><input name="patStep" value="` + (typeof value["step"] === "undefined" ? "1" : value["step"]) + `" type="number" required=true size=20 /></div>`;
     if (value["pattern"] == "RubberBand") {
-      html += `  <tr><td><div class="pattFormRow"><label>Center</label></td><td><input name="patCenter" value="` + (typeof value["center"] === "undefined" ? "50" : value["center"]) + `" type="number" required=true size=20 /></div></td></tr>`;
+      html += `  <div class="pattFormRow"><label>Center</label><input name="patCenter" value="` + (typeof value["center"] === "undefined" ? "50" : value["center"]) + `" type="number" required=true size=20 /></div>`;
     }
-    html += `  <tr><td><div class="pattFormRow"><label>Maximum</label></td><td><input name="patMax" value="` + (typeof value["max"] === "undefined" ? "0" : value["max"]) + `" type="number" required=true size=20 /></div></td></tr>`;
-    html += `  <tr><td><div class="pattFormRow"><label>MemberID</label></td><td><input name="patMemberID" value="` + value["MetricValueTemplate"]["MemberID"] + `" type="text" required=true size=80 unique/></div></td></tr>`;
-    html += `  <tr><td><div class="pattFormRow"><label>MetricProperty</label></td><td><input name="patMetricProp" value="` + value["MetricValueTemplate"]["MetricProperty"] + `" type="text" required=true size=80 unique/></div></td></tr>`;
-    html += `</table>`;
-	html += `<div><input type="button" value="-" id=value["name"] /></div>`
+    html += `  <div class="pattFormRow"><label>Maximum</label><input name="patMax" value="` + (typeof value["max"] === "undefined" ? "0" : value["max"]) + `" type="number" required=true size=20 /></div>`;
+    html += `  <div class="pattFormRow"><label>MemberID</label><input name="patMemberID" value="` + value["MetricValueTemplate"]["MemberID"] + `" type="text" required=true size=80 unique/></div>`;
+    html += `  <div class="pattFormRow"><label>MetricProperty</label><input name="patMetricProp" value="` + value["MetricValueTemplate"]["MetricProperty"] + `" type="text" required=true size=80 unique/></div>`;
+    html += `<div><input type="button" value="-" id=value["name"] /></div>`
     html += `</div><br /><hr />`;
   });
 
