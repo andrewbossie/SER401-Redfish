dce30332aac0d93bb73fcc0a1e151f342f7dcf9b
Wyatt Draggoo <wdraggoo@asu.edu>
2019-04-23 10:18:29 -0700

    Fixed default values in saves, and center value for rubberband

--- rfInsight/resources/js/rfModeller.js
+++ rfInsight/resources/js/rfModeller.js
@@ -32,13 +32,20 @@ $.when(
   populatePatterns();
 });
 
+function checkPatternProperties(select, parentDiv) {
+  var center = $(parentDiv).find('input[name="patCenter"]');
+
+  if ($(select).val() == "rubberband") {
+    $(center).prop('disabled', false);
+  } else {
+    $(center).prop('disabled', true);
+  }
+}
+
 function populatePatterns() {
   let html = "";
 
-  console.log(patternData);
-
   $.each(patternData["MockupData"]["MockupPatterns"], function(index, value) {
-    console.log(value);
     html += `<div class="pattern" id="patternDiv` + index + `">`;
     html += `  <div class="pattFormRow"><label>Enabled</label><input name="patEnabled" value="enabled" type="checkbox" ` + (typeof value["enabled"] === "undefined" ? "" : (value["enabled"] == true ? "checked" : "")) + ` size=80/></div>`;
     html += `  <div class="pattFormRow"><label>Name</label><input name="patName" value="` + value["name"] + `" type="text" required=true size=80 unique/></div>`;
@@ -47,10 +54,8 @@ function populatePatterns() {
     html += `  <div class="pattFormRow"><label>Delay</label><input name="patDelay" value="` + (typeof value["timedelay"] === "undefined" ? "15" : value["timedelay"]) + `" type="number" required=true size=20 /></div>`;
     html += `  <div class="pattFormRow"><label>Minimum</label><input name="patMin" value="` + (typeof value["min"] === "undefined" ? "0" : value["min"]) + `" type="number" required=true size=20 /></div>`;
     html += `  <div class="pattFormRow"><label>Step</label><input name="patStep" value="` + (typeof value["step"] === "undefined" ? "1" : value["step"]) + `" type="number" required=true size=20 /></div>`;
-    if (value["pattern"] == "RubberBand") {
-      html += `  <div class="pattFormRow"><label>Center</label><input name="patCenter" value="` + (typeof value["center"] === "undefined" ? "50" : value["center"]) + `" type="number" required=true size=20 /></div>`;
-    }
-    html += `  <div class="pattFormRow"><label>Maximum</label><input name="patMax" value="` + (typeof value["max"] === "undefined" ? "0" : value["max"]) + `" type="number" required=true size=20 /></div>`;
+    html += `  <div class="pattFormRow"><label>Center</label><input name="patCenter" value="` + (typeof value["center"] === "undefined" ? "50" : value["center"]) + `" type="number" required=true size=20 /></div>`;
+    html += `  <div class="pattFormRow"><label>Maximum</label><input name="patMax" value="` + (typeof value["max"] === "undefined" ? "100" : value["max"]) + `" type="number" required=true size=20 /></div>`;
     html += `  <div class="pattFormRow"><label>MemberID</label><input name="patMemberID" value="` + value["MetricValueTemplate"]["MemberID"] + `" type="text" required=true size=80 unique/></div>`;
     html += `  <div class="pattFormRow"><label>MetricProperty</label><input name="patMetricProp" value="` + value["MetricValueTemplate"]["MetricProperty"] + `" type="text" required=true size=80 unique/></div>`;
     html += `<div><input type="button" value="Remove" id=value["name"] onClick="function rm(){$('#pattern` + index + `').remove()}; rm()" /></div>`
@@ -68,8 +73,15 @@ function populatePatterns() {
     }
 
     $(select).val(value["pattern"]);
-  });
 
+    var parentDiv = $(select).parent().parent();
+
+    checkPatternProperties(select, parentDiv);
+
+    select.addEventListener('change', (event, select, parentDiv) => {
+      checkPatternProperties(select, parentDiv);
+    });
+  });
 }
 
 function buildJSONFromForm() {
@@ -92,6 +104,10 @@ function buildJSONFromForm() {
         "TimeStamp": "#timestamp",
         "MetricProperty": $(this).find('input[name="patMetricProp"]').val()
       }
+    }
+
+    if (pattern["pattern"] == "rubberband") {
+      pattern["center"] = $(this).find('input[name="patCenter"]').val();
     }
 
     config["MockupData"]["MockupPatterns"].push(pattern);
